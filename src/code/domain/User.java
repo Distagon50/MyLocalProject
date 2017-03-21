d6f184e49d132e7fa9bed43b7caad00a9c369bd1
Distagon50 <liganyu84@hotmail.com>
2017-03-21 18:21:07 +0800

    Sumit the java code

---
+++ src/code/domain/User.java
@@ -0,0 +1,53 @@
+package code.domain;
+
+public class User {
+	
+	private Long id;
+	
+	private String name;
+	
+	private int age;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ src/code/repository/CoreRepository.java
@@ -0,0 +1,5 @@
+package code.repository;
+
+public class CoreRepository {
+
+}
